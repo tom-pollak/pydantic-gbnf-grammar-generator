e51836d5657818e49241cb188173e1a4bd2a3252
Tom Pollak <tompollak1000@gmail.com>
2025-03-11 15:15:27 +0000

    almost!

--- src/pydantic_gbnf_grammar_generator/main.py
+++ src/pydantic_gbnf_grammar_generator/main.py
@@ -100,7 +100,7 @@ def generate_list_rule(element_type):
     """
     rule_name = f"{map_pydantic_type_to_gbnf(element_type)}-list"
     element_rule = map_pydantic_type_to_gbnf(element_type)
-    list_rule = rf'{rule_name} ::= nl "<items>" nl ("<item>" nl {element_rule} nl "</item>")* nl "</items>"'
+    list_rule = rf'{rule_name} ::= nl "<items>" ("<item>" {element_rule} nl "</item>")* nl "</items>"'
     return list_rule
 
 
@@ -113,7 +113,7 @@ def get_members_structure(cls, rule_name):
         result = f'{rule_name} ::= nl "<{rule_name}>" nl'
         # Modify for XML structure with newlines after opening and before closing tags
         members = [
-            f'nl "<{name}>" nl {map_pydantic_type_to_gbnf(param_type)} nl "</{name}>"'
+            f'nl "<{name}>" {map_pydantic_type_to_gbnf(param_type)} nl "</{name}>"'
             for name, param_type in cls.__annotations__.items()
             if name != "self"
         ]
@@ -131,7 +131,7 @@ def get_members_structure(cls, rule_name):
     result = f'{rule_name} ::= nl "<{rule_name}>" nl'
     # Modify for XML structure with newlines after opening and before closing tags
     members = [
-        f'nl "<{name}>" nl {map_pydantic_type_to_gbnf(param.annotation)} nl "</{name}>"'
+        f'nl "<{name}>" {map_pydantic_type_to_gbnf(param.annotation)} nl "</{name}>"'
         for name, param in parameters.items()
         if name != "self" and param.annotation != inspect.Parameter.empty
     ]
@@ -326,7 +326,7 @@ def generate_gbnf_rule_for_type(
             model_name, f"{field_name}Element", element_type, is_optional, processed_models, created_rules
         )
         rules.extend(additional_rules)
-        array_rule = f"""{model_name}{field_name} ::= nl "<items>" nl ("<item>" nl {element_rule_name} nl "</item>")* nl "</items>" """
+        array_rule = f"""{model_name}{field_name} ::= nl "<items>" ("<item>" {element_rule_name} nl "</item>")* nl "</items>" """
         rules.append(array_rule)
         gbnf_type, rules = model_name + field_name, rules
 
@@ -336,7 +336,7 @@ def generate_gbnf_rule_for_type(
             model_name, f"{field_name}Element", element_type, is_optional, processed_models, created_rules
         )
         rules.extend(additional_rules)
-        array_rule = f"""{model_name}{field_name} ::= nl "<items>" nl ("<item>" nl {element_rule_name} nl "</item>")* nl "</items>" """
+        array_rule = f"""{model_name}{field_name} ::= nl "<items>" ("<item>" {element_rule_name} nl "</item>")* nl "</items>" """
         rules.append(array_rule)
         gbnf_type, rules = model_name + field_name, rules
 
@@ -351,7 +351,7 @@ def generate_gbnf_rule_for_type(
         additional_value_type, additional_value_rules = generate_gbnf_rule_for_type(
             model_name, f"{field_name}-value-type", value_type, is_optional, processed_models, created_rules
         )
-        gbnf_type = rf'{gbnf_type} ::= nl "<dictionary>" nl ("<entry>" nl "<key>" nl {additional_key_type} nl "</key>" nl "<value>" nl {additional_value_type} nl "</value>" nl "</entry>")* nl "</dictionary>" '
+        gbnf_type = rf'{gbnf_type} ::= nl "<dictionary>" ("<entry>" nl "<key>" {additional_key_type} nl "</key>" nl "<value>" {additional_value_type} nl "</value>" nl "</entry>")* nl "</dictionary>" '
 
         rules.extend(additional_key_rules)
         rules.extend(additional_value_rules)
@@ -531,14 +531,14 @@ def generate_gbnf_grammar(
             if rule_name not in created_rules:
                 created_rules[rule_name] = additional_rules
             # XML Format with newlines after opening tags and before closing tags
-            model_rule_parts.append(f'nl "<{field_name}>" nl {rule_name} nl "</{field_name}>"' )
+            model_rule_parts.append(f'nl "<{field_name}>" {rule_name} nl "</{field_name}>"' )
             nested_rules.extend(additional_rules)
         else:
             has_triple_quoted_string = look_for_triple_quoted_string
             has_markdown_code_block = look_for_markdown_code_block
 
     fields_joined = ' '.join(model_rule_parts)
-    model_rule = rf'{model_name} ::= nl "<{model_name}>" nl {fields_joined} nl "</{model_name}>"'
+    model_rule = rf'{model_name} ::= nl "<{model_name}>" {fields_joined} nl "</{model_name}>"'
 
     has_special_string = False
     if has_triple_quoted_string:
